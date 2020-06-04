60114bb2c61c73cd248bc2e90bf56684b5e53af5
Lucas Siqueira Fernandes <lucassiqueirafernandes@MacBook-Air-de-Lucas.local>
2020-06-04 12:40:19 -0300

    setting react final application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
+import Routes from './routes';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World
-        </p>
-      </header>
+    <div>
+      <Routes />
     </div>
   );
 }
